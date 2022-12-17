c1b47c072739c33df690ea9b1a044ca8f66a6675
hakjun-kim <hakjun-kim@linecorp.com>
2022-12-18 03:27:49 +0900

    java 풀이 변경

---
+++ src/test/java/com/example/codingtest/Programmers/level1/가장가까운글자/Java.java
@@ -0,0 +1,38 @@
+package com.example.codingtest.Programmers.level1.가장가까운글자;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Java {
+
+    @Test
+    void main() {
+        int[] result = solution("banana");
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        Map<Character, Integer> charPositionMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int position = charPositionMap.getOrDefault(c, -1);
+            charPositionMap.put(c, i);
+
+            if (position != -1) {
+                position = i - position;
+            }
+
+            answer[i] = position;
+        }
+
+        return answer;
+    }
+
+
+}
---
+++ src/test/java/com/example/codingtest/Programmers/level1/가장가까운글자/Kotlin.kt
@@ -0,0 +1,19 @@
+package com.example.codingtest.Programmers.level1.가장가까운글자
+
+import org.junit.jupiter.api.Test
+
+class Kotlin {
+
+    @Test()
+    fun main(){
+        var result = solution("banana")
+        println(result);
+    }
+
+    fun solution(s: String): List<Int> {
+
+
+        return listOf()
+    }
+
+}
